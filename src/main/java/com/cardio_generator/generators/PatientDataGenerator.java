76ec9472907a373e2a77e47b43538e84071de9ff
Alibrandi, Omar (Stud. DACS) <o.alibrandi@student.maastrichtuniversity.nl>
2025-04-05 14:23:46 +0200

    Javadoc documention following google's java style guide of the first 3 out of 7 on the list

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,19 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * Interface for generating patient health data.
+ *
+ * <p>Classes implementing this interface are responsable for generating specific
+ * types of health data for patients and outputting it through the provided strategy
+ */
 public interface PatientDataGenerator {
+
+    /**
+     * Generates health data for a specific patient.
+     *
+     * @param patientId the unique ID of the patient for whom data is being generated
+     * @param outputStrategy the strategy to use for outputting the data generated
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,20 @@
 package com.cardio_generator.outputs;
 
+/**
+ * Strategy interface for outputting patient health data.
+ *
+ * <p>The interface provides a strategy for different output mechanisms
+ * that can be used to handle patient health data.
+ */
 public interface OutputStrategy {
+
+    /**
+     * Outputs patient health data.
+     *
+     * @param patientId the unique ID of the patient
+     * @param timestamp the time when the data was generated in milliseconds
+     * @param label the type or category of health data (e.g., "ECG", "Blood Pressure")
+     * @param data the actual health data in string format
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
